a1d6cd59d5d5f2a129abdb98bf8c34eba0d8591a
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-09-16 22:20:16 +0530

    Added Complete signup flow

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,6 +3,8 @@ import HomeLayout from "../Layouts/HomeLayout"
 import { BsPersonCircle } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { toast } from 'react-hot-toast';
+import { createAccount } from "../Redux/Slices/AuthSlice";
 
 function Signup() {
 
@@ -30,12 +32,71 @@ function Signup() {
         event.preventDefault();
         // getting the image
         const uploadImage = event.target.files[0];
+
+        if (uploadImage) {
+            setSignupData({
+                signupData,
+                avatar: uploadImage
+            });
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(uploadImage);
+            fileReader.addEventListener("load", function() {
+                // console.log(this.result);
+                setPreviewImage(this.result)
+            })
+        }
+    }
+
+    async function createNewAccount(event) {
+        event.preventDefault();
+        if (!signupData.email || !signupData.password || !signupData.fullName || !signupData.avatar) {
+            toast.error("Please fill all the details");
+            return;
+        }
+
+        // checking name field length
+        if (signupData.fullName.length < 5) {
+            toast.error("Name should be atleast of 5 characters")
+            return;
+        }
+
+        // checking valid email
+        if (!signupData.email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            toast.error("Invalid email id")
+            return;
+        }
+
+        // checking password validation
+        if (!signupData.password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)) {
+            toast.error("Password should be 6-16 char long & atleast one special char")
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("fullName", signupData.fullName);
+        formData.append("email", signupData.email);
+        formData.append("password", signupData.password);
+        formData.append("avatar", signupData.avatar);
+
+        // dispatch create account action
+        const response = await dispatch(createAccount(formData));
+        console.log(response);
+        if (response?.payload?.success)
+            navigate("/"); 
+
+        setSignupData({
+            fullName: "",
+            email: "",
+            password: "",
+            avatar: ""
+        });
+        setPreviewImage("");
     }
 
     return (
         <HomeLayout>
             <div className="flex items-center justify-center h-[90vh]">
-                <form className="flex flex-col justify-center gap-3 rounded-lg p-4 text-yellow-500 w-96 shadow-[0_0_10px_black]">
+                <form noValidate onSubmit={createNewAccount} className="flex flex-col justify-center gap-3 rounded-lg p-4 text-yellow-500 w-96 shadow-[0_0_10px_black]">
                     <h1 className="text-center text-2xl font-bold">Registration Page</h1>
 
                     <label htmlFor="image_uploads" className="cursor-pointer">
@@ -46,10 +107,11 @@ function Signup() {
                         )}
                     </label>
                     <input
+                        onChange={getImage}
                         className="hidden"
                         type="file"
                         name="image_uploads"
-                        id="image_upload"
+                        id="image_uploads"
                         accept=".jpg, .jpeg, .png, .svg"
                      />
 
