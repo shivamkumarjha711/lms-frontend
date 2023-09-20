48d971d6f5f217ee8467fdbd59ed2f902995897d
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-09-20 21:16:00 +0530

    Contact page complete

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { toast } from 'react-hot-toast';
 import { createAccount } from "../Redux/Slices/AuthSlice";
+import { isEmail, isValidPassword } from "../Helpers/regexMatcher";
 
 function Signup() {
 
@@ -61,13 +62,13 @@ function Signup() {
         }
 
         // checking valid email
-        if (!signupData.email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        if (!isEmail(signupData.email)) {
             toast.error("Invalid email id")
             return;
         }
 
         // checking password validation
-        if (!signupData.password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)) {
+        if (!isValidPassword(signupData.password)) {
             toast.error("Password should be 6-16 char long & atleast one special char")
             return;
         }
