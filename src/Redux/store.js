6cf2b0081b5723ba1d3414ff3001a745e8d81d58
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-09-24 13:17:40 +0530

    Added lecture slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from './Slices/AuthSlice';
 import CourseSliceReducer from './Slices/CourseSlice';
 import razorpaySliceReducer from './Slices/RazorpaySlice';
+import lectureSliceReducer from './Slices/LectureSlice';
 
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
         course: CourseSliceReducer,
-        razorpay: razorpaySliceReducer
+        razorpay: razorpaySliceReducer,
+        lecture: lectureSliceReducer
     },
     devTools: true
 })
